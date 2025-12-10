eb592814d4efd573c2b952daa8a0998c40cca7b4
LarissaOlli <larissalaiolysilva@outlook.com>
2025-12-10 11:50:14 -0300

    Salvando os dados do IMC dos pacientes

--- exemplos-python/12-imc.py
+++ exemplos-python/12-imc.py
@@ -4,31 +4,58 @@ import os
 #limpando a tela
 os.system("cls")
 
-nome = input("Digite seu nome: ")
-peso = float(input("Digite seu peso: "))
-altura = float(input("Digite sua altura: "))
+resposta = "sim"
 
-imc = peso / (altura * altura)
+while (resposta =="sim"):
 
-print(f"Olá {nome}, seu IMC é: {round(imc,2)}")
+    nome = input("Digite seu nome: ")
+    peso = float(input("Digite seu peso: "))
+    altura = float(input("Digite sua altura: "))
+
+    imc = peso / (altura * altura)
+
+    imc_dos_pacientes = ""
+
+    print(f"Olá {nome}, seu IMC é: {round(imc,2)}")
 
 
-if imc <=18.5:
-    print("Você está abaixo do peso.")
+    if imc <=18.5:
+        print("Você está abaixo do peso.")
+        imc_dos_pacientes = "Você está abaixo do peso."
 
-elif imc >=24.9:
-    print("Peso normal.")
+    elif imc >=24.9:
+        print("Peso normal.")
+        imc_dos_pacientes = "Peso normal."
 
-elif imc <=29.9:
-    print("Sobrepeso.")
+    elif imc <=29.9:
+        print("Sobrepeso.")
+        imc_dos_pacientes = "Sobrepeso."
 
-elif imc <=34.9:
-    print("Obesidade grau I") 
+    elif imc <=34.9:
+        print("Obesidade grau I") 
+        imc_dos_pacientes = "Obesidade grau I"
 
-elif imc <=39.9:
-    print("Obesidade grau II")
+    elif imc <=39.9:
+        print("Obesidade grau II")
+        imc_dos_pacientes = "Obesidade grau II"
 
-elif imc >=40:
-    print("Obessidade grau III")
+    elif imc >=40:
+        print("Obessidade grau III")
+        imc_dos_pacientes = "Obessidade grau III"
+    
+    with open("imc_dos_pacientes.txt","a",encoding="utf-8") as arquivo:
+        arquivo.write(f"Nome do Paciente: {nome} \n")
+        arquivo.write(f"Peso do Paciente: {peso}\n")
+        arquivo.write(f"Altura do Paciente: {altura}\n")
+        arquivo.write(f"Olá {nome}, seu IMC é: {imc_dos_pacientes}\n")
+        arquivo.write("========================================================= \n")
+
+        print("IMC feito com sucesso!!")
+    
+    resposta = input("Gostaria de executar novamente, sim ou não?: ")
+    
+    os.system("cls")
+   
 
 input("Pressione <Enter> para fechar o programa")
+
