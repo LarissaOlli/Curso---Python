0134541699135aba9ab7c41cf2caabbfeed7d773
LarissaOlli <larissalaiolysilva@outlook.com>
2025-12-10 11:45:37 -0300

    Salvando os dados dos alunos
    Modificando o arquivo para salvar os dados dos alunos em um arquivo de texto.

--- exemplos-python/11-media.py
+++ exemplos-python/11-media.py
@@ -14,25 +14,49 @@ print("""
 ╚═════╝░░╚════╝░╚══════╝╚══════╝░░░╚═╝░░░╚═╝╚═╝░░░░░╚═╝
       """)
 
-nome = input("Digite seu nome: ")
-nota01 = float (input("Digite sua primeira nota: "))
-nota02 = float (input("Digite sua segunda nota: "))
-nota03 = float (input("Digite sua terceira nota: "))
+resposta = "sim"
 
-media = (nota01 + nota02 + nota03) / 3
+while (resposta == "sim"):
 
-print(f"Sua média foi {round(media,2)}")
+    nome = input("Digite seu nome: ")
+    nota01 = float (input("Digite sua primeira nota: "))
+    nota02 = float (input("Digite sua segunda nota: "))
+    nota03 = float (input("Digite sua terceira nota: "))
+
+    media = (nota01 + nota02 + nota03) / 3
+
+    situaçao_do_aluno = ""
+
+    print(f"Sua média foi {round(media,2)}")
 
 
-if media >=7:
-    print("Aprovado")
+    if media >=7:
+        print("Aprovado")
+        situaçao_do_aluno = "Aprovado"
+        
+    elif media >4:
+        print ("Em recuperação")
+        situaçao_do_aluno = "Recuperação"
+
+    else:
+        print("Reprovado")
+        situaçao_do_aluno = "Reprovado"
+
+    # Gravar os dados dos alunos
+    with open("notas_dos_alunos.txt","a",encoding="utf-8") as arquivo:
+        arquivo.write(f"Nome do Aluno: {nome} \n")
+        arquivo.write(f"Nota 01: {nota01}\n")
+        arquivo.write(f"Nota 02: {nota02}\n")
+        arquivo.write(f"Nota 03: {nota03}\n")
+        arquivo.write(f"Média do Aluno: {media}\n")
+        arquivo.write(f"Situação do Aluno: {situaçao_do_aluno}\n")
+        arquivo.write("========================================================= \n")
+
+        print("Dados salvos com sucesso!!")
     
-elif media >4:
-    print ("Em recuperação")
+    resposta = input("Gostaria de executar novamente, sim ou não?: ")
+    
+    os.system("cls")
 
-else:
-    print("Reprovado")
-
-input("Pressione qualquer <Enter> para fechar o programa.")    
-
+input("Pressione qualquer <Enter> para fechar o programa.") 
       
